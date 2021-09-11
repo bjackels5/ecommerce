e19bb4496ef41db7ee9cdd2f68953073b87e5ad5
bjackels5 <85116312+bjackels5@users.noreply.github.com>
2021-09-11 14:23:02 -0400

    define model relationships

--- models/index.js
+++ models/index.js
@@ -8,12 +8,29 @@ const ProductTag = require('./ProductTag');
 // Where did they do that?
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+    foreignKey: 'category_id'
+});
+
 
 // Categories have many Products
+Category.hasMany(Product, {
+    foreignKey: 'category_id'
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+    through: ProductTag,
+    as: 'tagged_products',
+    foreignKey: 'product_id'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+    through: ProductTag,
+    as: 'tagged_products',
+    foreignKey: 'tag_id'
+});
 
 module.exports = {
   Product,
