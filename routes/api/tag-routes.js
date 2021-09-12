22a326591192f1fc9a4d8e4c2dd6e66c21b381a3
bjackels5 <85116312+bjackels5@users.noreply.github.com>
2021-09-12 11:01:23 -0400

    added functionality to delete a tag

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -94,6 +94,22 @@ router.put('/:id', (req, res) => {
 // /api/tags/1
 router.delete('/:id', (req, res) => {
     // delete on tag by its `id` value
+    Tag.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbData => {
+        if (!dbData) {
+            res.status(404).json({ message: 'No tag found with this id' });
+            return;
+        }
+        res.json(dbData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
 });
 
 module.exports = router;
